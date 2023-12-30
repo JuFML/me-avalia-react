8d956cb7213b2e30f7b7e640ed044738ef0a8a5e
JuFML <julianafernandezlima@hotmail.com>
2023-12-30 19:41:52 -0300

    Marcacao boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+
 const App = () => {
-  return <h1>Boilerplate Vite + React</h1>;
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch("./src/a.json")
+      .then(data => data.json())
+      .then(resp => setMovies(resp))
+      .catch(console.log)
+  }, [])
+  return (
+    <>
+      <Navbar/>
+      <main className="main">
+        <div className="box">
+          <ul className="list list-movies">
+            {movies.map(movie => (
+              <li>
+                <img src={movie.Poster} alt="" />
+                <h3>{movie.Title}</h3>
+                <p>📅 {movie.Year}</p>
+            </li>
+            ))}
+          </ul>
+        </div>
+        <div className="box">
+          <div className="summary">
+            <h2>filmes assistidos</h2>
+            <div>
+              <p>#️⃣ 0 filmes</p>
+              <p>⏳ 0 minutos</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 };
 
 export default App;
