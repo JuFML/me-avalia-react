71aabd25913d79151aedb10d14c0b85165ce3e15
JuFML <julianafernandezlima@hotmail.com>
2023-12-30 19:40:37 -0300

    Marcacao navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+      <section className="nav-bar">
+        <div>LOGO</div>
+        <div>BUSCA</div>
+        <div>RESULTADOS</div>
+      </section>
+    )
+  };
+  
+  export default Navbar;
