197d0fb5982f69817de3d027bf0b34856f2eb279
JuFML <julianafernandezlima@hotmail.com>
2023-12-31 09:02:23 -0300

    Funcao de busca adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
 import { useEffect } from "react";
-import Navbar from "./components/Navbar";
 import { useState } from "react";
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [inputSearch, setInputSearch] = useState("")
+  const [searchedMovies, setSearchedMovies] = useState([])
   
   useEffect(() => {
     fetch("./src/a.json")
       .then(data => data.json())
-      .then(resp => setMovies(resp))
+      .then(resp => {
+        setMovies(resp)
+        setSearchedMovies(resp)
+      })
       .catch(console.log)
   }, [])
+
+  useEffect(() => {
+    if(inputSearch === "") {
+      setSearchedMovies([...movies])
+    }
+  }, [inputSearch])
+ 
+
+  const handleSearchChange = (e) => {
+    setInputSearch(e.target.value.toLowerCase())
+  }
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    setSearchedMovies(movies.filter(movie => {
+      return movie.Title.toLowerCase().includes(inputSearch)
+    }))
+  }
   return (
     <>
-      <Navbar movies={movies}/>
+      <section className="nav-bar">
+        <img className="logo" src="/images/logo-me-avalia.png" alt="" />
+        <form className="form-search" onSubmit={handleSearchSubmit}>
+          <input value={inputSearch} className="search" type="text" placeholder="Buscar filmes..." onChange={handleSearchChange}/>
+          <button className="btn-search">Buscar</button>
+        </form>
+        <div className="num-results"><strong>{movies.length}</strong> Resultados</div>
+      </section>
       <main className="main">
         <div className="box">
           <ul className="list list-movies">
-            {movies.map(movie => (
+            {searchedMovies.map(movie => (
               <li key={movie.imdbID}>
                 <img src={movie.Poster} alt="" />
                 <h3>{movie.Title}</h3>
