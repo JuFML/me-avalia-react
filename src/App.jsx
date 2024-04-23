ada6f4ec6608ddfefc8eb9229d4c641c46e7812f
JuFML <julianafernandezlima@hotmail.com>
2024-04-23 09:59:56 -0300

    ✅  create useClickedMovie file

--- src/App.jsx
+++ src/App.jsx
@@ -6,50 +6,12 @@ import { Movies } from "./components/Movies";
 import { WatchedMovies } from "./components/WatchedMovies";
 import { MovieDetails } from "./components/MovieDetails";
 import { useWatchedMovie } from "./hooks/useWatchedMovies";
+import { useClickedMovie } from "./hooks/useClickedMovie";
 import { apiKey } from "./utils/apiKey"
 
 const ListBox = ({children}) => <div className="box">{children}</div>
 
-const useClickedMovie = (watchedMovies, setWatchedMovies) => {
-  const [clickedMovie, setClickedMovie] = useState([])
-  const [rating, setRating] = useState("")
-  const [tempRating, setTempRating] = useState("")
 
-  const handleBackClick = () => {
-    setClickedMovie([])
-    setRating()
-  }
-  const handleRatingClick = (rating) => setRating(rating)
-  const handleMovieClick = (movie) => {
-    const isFilmOnList = watchedMovies.find(film => film.imdbID == movie.imdbID)
-    const isTheSameFilm = movie.imdbID == clickedMovie.imdbID
-    if(isFilmOnList || isTheSameFilm) {
-      return
-    }
-
-    const idMovieClicked = (movie.imdbID);
-
-    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${idMovieClicked}`)
-      .then(data => data.json())
-      .then(resp => {
-        if(resp.Runtime === "N/A") {
-          setClickedMovie({...resp, Runtime: "0 min"})
-          return
-        }
-        setClickedMovie(resp)
-      })
-      .catch(error => alert(error.message))
-  }
-  const handleAddFilm = () => {
-    setWatchedMovies(prev => [...prev, {...clickedMovie, rate: rating}])
-    setClickedMovie([])
-    setRating("")
-  }
-  const handleMouseEnter = (rating) => {setTempRating(rating)}
-  const handleMouseLeave = () => {setTempRating("")}
-
-  return {clickedMovie, setClickedMovie, handleBackClick, handleRatingClick, handleMovieClick, handleAddFilm, rating, handleMouseEnter, handleMouseLeave, tempRating}
-}
 
 const Main = ({movies}) => {
   const {watchedMovies, setWatchedMovies, minutesWatched, handleDeleteClick} = useWatchedMovie()
---
+++ src/hooks/useClickedMovie.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { apiKey } from "../utils/apiKey"
+
+const useClickedMovie = (watchedMovies, setWatchedMovies) => {
+  const [clickedMovie, setClickedMovie] = useState([])
+  const [rating, setRating] = useState("")
+  const [tempRating, setTempRating] = useState("")
+
+  const handleBackClick = () => {
+    setClickedMovie([])
+    setRating()
+  }
+  const handleRatingClick = (rating) => setRating(rating)
+  const handleMovieClick = (movie) => {
+    const isFilmOnList = watchedMovies.find(film => film.imdbID == movie.imdbID)
+    const isTheSameFilm = movie.imdbID == clickedMovie.imdbID
+    if (isFilmOnList || isTheSameFilm) {
+      return
+    }
+
+    const idMovieClicked = (movie.imdbID);
+
+    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${idMovieClicked}`)
+      .then(data => data.json())
+      .then(resp => {
+        if (resp.Runtime === "N/A") {
+          setClickedMovie({ ...resp, Runtime: "0 min" })
+          return
+        }
+        setClickedMovie(resp)
+      })
+      .catch(error => alert(error.message))
+  }
+  const handleAddFilm = () => {
+    setWatchedMovies(prev => [...prev, { ...clickedMovie, rate: rating }])
+    setClickedMovie([])
+    setRating("")
+  }
+  const handleMouseEnter = (rating) => { setTempRating(rating) }
+  const handleMouseLeave = () => { setTempRating("") }
+
+  return { clickedMovie, setClickedMovie, handleBackClick, handleRatingClick, handleMovieClick, handleAddFilm, rating, handleMouseEnter, handleMouseLeave, tempRating }
+}
+
+export { useClickedMovie }
